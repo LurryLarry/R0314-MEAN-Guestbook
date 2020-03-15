50b1b6ed463a2f011f8d0ffd57470527841f383c
LurryLarry <lari.vesterinen@gmail.com>
2020-03-15 19:23:50 +0200

    beta versio

---
+++ index.js
@@ -0,0 +1,98 @@
+const axios = require("axios");
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const http = require("http");
+const bodyParser = require("body-parser");
+
+// const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.use(express.static("public"));
+app.use("/api", express.static("api"), function(req, res) {
+    // Optional 404 handler
+    res.status(404);
+    res.json({ error: { code: 404 } });
+});
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/guestbook", function(req, res) {
+    res.sendFile(__dirname + "/public/guestbook.html");
+});
+
+app.get("/newmessage", function(req, res) {
+    res.sendFile(__dirname + "/public/newmessage.html");
+});
+
+app.get("/newmessageajax", function(req, res) {
+    res.sendFile(__dirname + "/public/newmessageajax.html");
+});
+
+app.get("*", function(req, res) {
+    res.send("Cant find the requested page", 404);
+});
+
+let data = require(__dirname + "/public/api/data.json");
+
+app.post("/update", function(req, res) {
+    let date = new Date();
+    date = date.toUTCString();
+
+    let entry = {
+        "id": data.length + 1,
+        "username": req.body.name,
+        "country": req.body.country,
+        "date": date,
+        "message": req.body.message
+    };
+
+    data.push(entry);
+
+    fs.writeFile(__dirname + "/public/api/data.json", JSON.stringify(data), function(err) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.");
+            return console.log(err);
+        }
+        console.log("JSON file has been saved.");
+    });
+    res.sendFile(__dirname + '/public/guestbook.html');
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/update-with-ajax', function(req, res) {
+    console.log(req.body.name);
+
+    let date = new Date();
+    date = date.toUTCString();
+
+    let entry = {
+        "id": data.length + 1,
+        "username": req.body.name,
+        "country": req.body.country,
+        "date": date,
+        "message": req.body.message
+    };
+
+    data.push(entry);
+
+    fs.writeFile(__dirname + "/public/api/data.json", JSON.stringify(data), function(err) {
+        if (err) {
+            console.log("An error occured while writing JSON Object to File.");
+            return console.log(err);
+        }
+        console.log("JSON file has been saved.");
+    });
+    res.end();
+
+});
+
+app.listen(8081, function() {
+    console.log("Example app listening on port 8081!");
+});
---
+++ public/javascript/ajaxmessage.js
@@ -0,0 +1,68 @@
+window.onload = () => {
+    $('button').on('click', function(e) {
+        e.preventDefault();
+
+
+
+        var url = "/api/data.json";
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
+
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                let data = JSON.parse(xmlhttp.responseText);
+                let table = `<table align="center" class="pure-table pure-table-horizontal" >
+    <thead>
+        <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Country</th>
+            <th>Message</th>
+            <th>Date</th>
+        </tr>
+    </thead><tbody>`;
+
+                for (let i = 0; i < data.length; i++) {
+                    table +=
+                        "<tr><td style='font-weight: bold;'>" +
+                        data[i].id +
+                        "</td><td>" +
+                        data[i].username +
+                        "</td><td>" +
+                        data[i].country +
+                        "</td><td>" +
+                        data[i].message +
+                        "</td><td>" +
+                        data[i].date +
+                        "</td>";
+                }
+
+                table += "</tbody></table>";
+                console.log(table);
+
+                var output = document.getElementById("table");
+
+
+
+                output.innerHTML = table;
+
+                let name = document.getElementById("name").value;
+                let country = document.getElementById("country").value;
+                let message = document.getElementById("comment").value;
+
+                $.post({
+                    async: false,
+                    type: "POST",
+                    url: "/update-with-ajax",
+                    dataType: "json",
+                    data: {
+                        "name": name,
+                        "country": country,
+                        "message": message
+                    }
+                });
+            }
+        };
+    });
+};
